05e10492173f5cff9ca93f71bc951cb5fa2dd734
DanielMChristiansen <danielmchristiansen@outlook.com>
2025-02-25 21:20:54 -0700

    Added basic login and registation reactivity to login page

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,24 +1,64 @@
 import React from "react";
 import '../login.css'
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Login() {
-    return (
-      <main id="loginPage">
-        <div className="shrinker">
-          <input id="emailInput" type="email" placeholder=" 📧 Email:" className="loginField" /><br />
-          <input id="passwordInput" type="password" placeholder=" 🔒 Password:" className="loginField" /><br />
-          <div className="loginButtons">
-            <NavLink to="/ataglancePage">
-              <button id="loginButton">Login</button>
-            </NavLink>
-            <NavLink to="/ataglancePage">
-              <button id="registerButton">Register</button>
-            </NavLink>
-          </div>
+  const navigate = useNavigate()
+  function loginButtonClick() {
+    const email = document.getElementById("emailInput").value;
+    const password = document.getElementById("passwordInput").value;
+    tryLogin(email, password);
+  }
+
+  function validateEmail(email) {
+    var re = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+    return re.test(email);
+  }
+
+  function tryLogin(email, password) {
+    if (!validateEmail(email)) {
+      alert("Invalid email address");
+      return;
+    }
+    if (password === "") {
+      alert("Password cannot be empty");
+      return;
+    }
+    // Will interact with the backend here
+    alert.log(`Email: ${email}, Password: ${password}`);
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("authenticated", true);
+    navigate("/ataglancePage");
+  }
+
+  function registerButtonClick() {
+    const email = document.getElementById("emailInput").value;
+    const password = document.getElementById("passwordInput").value;
+    if (!validateEmail(email)) {
+      alert("Invalid email address");
+      return;
+    }
+    if (password === "") {
+      alert("Password cannot be empty");
+      return;
+    }
+    localStorage.setItem("email", document.getElementById("emailInput").value);
+    localStorage.setItem("authenticated", true);
+    navigate("/ataglancePage");
+  }
+  return (
+    <main id="loginPage">
+      <div className="shrinker">
+        <input id="emailInput" type="email" placeholder=" 📧 Email:" className="loginField" /><br />
+        <input id="passwordInput" type="password" placeholder=" 🔒 Password:" className="loginField" /><br />
+        <div className="loginButtons">
+          <button id="loginButton" onClick={loginButtonClick}>Login</button>
+          <button id="registerButton" onClick={registerButtonClick}>Register</button>
         </div>
-      </main>
-    )
+      </div>
+    </main>
+  )
 }  
 
 export default Login;
