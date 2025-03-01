aec4c742b01efc8f12bdf278f0c556ff6681b6aa
DanielMChristiansen <danielmchristiansen@outlook.com>
2025-03-01 00:20:15 -0700

    Moved AtAGlance.jsx for refactoring

--- index.jsx
+++ index.jsx
@@ -7,7 +7,7 @@ import './headerandfooter.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Login from './components/Login'
-import AtAGlance from './components/AtAGlance'
+import AtAGlance from './components/at_a_glance/AtAGlance'
 import Tutorial from './components/Tutorial'
 import Setup from './components/Setup'
 
