9e1ed26034881e7408a4d04bf3e7daba91ce7257
DanielMChristiansen <danielmchristiansen@outlook.com>
2025-03-13 10:25:13 -0600

    Migrated to use database

--- service/index.js
+++ service/index.js
@@ -4,12 +4,10 @@ const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const uuid = require("uuid");
+const DB = require("./database.js");
 
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
-
-let users = [];
-const passwords = {};
 
 app.use(express.json());
 app.use(cookieParser());
@@ -37,54 +35,45 @@ apiRouter.get("/corsbypass", cors(), (req, res) => {
 });
 
 apiRouter.post("/register", async (req, res) => {
-  if (passwords[req.body.email]) {
+  if (await findUser("email", req.body.email)) {
     res.status(400).send("User already exists");
     return;
   }
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const newToken = uuid.v4();
-  users.push({
-    email: req.body.email,
-    password: hashedPassword,
-    token: newToken,
-    completedAssignments: [],
-    calendars: [],
-  });
-  res.cookie("token", newToken);
+  const user = await createUser(req.body.email, hashedPassword);
+  await DB.addUser(user);
+  setAuthCookie(res, user.token);
   res.send({ user: req.body.email });
 });
 
 apiRouter.put("/login", async (req, res) => {
-  let user = users.find((user) => user.email === req.body.email);
-  // const hashedPassword = user.password;
+  let user = await findUser("email", req.body.email);
+
   if (
-    user &&
-    user.password &&
+    user?.password &&
     (await bcrypt.compare(req.body.password, user.password))
   ) {
-    const newToken = uuid.v4();
-    user.token = newToken;
-    res.cookie("token", newToken);
+    user.token = uuid.v4();
+    await DB.updateUser(user);
+    setAuthCookie(res, user.token);
     res.send({ user: req.body.email });
   } else {
     res.status(401).send("Invalid email or password");
   }
 });
 
-apiRouter.delete("/logout", (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
-  if (!user) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
+apiRouter.delete("/logout", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
+
   delete user.token;
+  await DB.updateUser(user);
   res.clearCookie("token");
   res.status(204).end();
 });
 
 // Middleware to verify that the user is authenticated
 const verifyAuth = async (req, res, next) => {
-  const user = users.find((user) => user.token === req.cookies.token);
+  const user = await findUser("token", req.cookies.token);
   if (user) {
     next();
   } else {
@@ -96,8 +85,8 @@ apiRouter.get("/authenticated", verifyAuth, (req, res) => {
   res.send({ authenticated: true });
 });
 
-apiRouter.put("/assignmentCompleted", verifyAuth, (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
+apiRouter.put("/assignmentCompleted", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
   let id = req.body.completedAssignmentId;
   if (req.body.done) {
     if (!user.completedAssignments.includes(id)) {
@@ -108,37 +97,71 @@ apiRouter.put("/assignmentCompleted", verifyAuth, (req, res) => {
       (assignmentId) => assignmentId !== id
     );
   }
+  await DB.updateUser(user);
   res.send({ completedAssignments: user.completedAssignments });
 });
 
-apiRouter.get("/completedAssignments", verifyAuth, (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
+apiRouter.get("/completedAssignments", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
   res.send({ completedAssignments: user.completedAssignments });
 });
 
 // Add calendar
-apiRouter.put("/calendars", verifyAuth, (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
+apiRouter.put("/calendars", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
   if (!user.calendars.includes(req.body.calendar)) {
     user.calendars.push(req.body.calendar);
+    DB.updateUser(user);
   }
   res.send({ calendars: user.calendars });
 });
 
 // Get calendars
-apiRouter.get("/calendars", verifyAuth, (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
+apiRouter.get("/calendars", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
   res.send({ calendars: user.calendars });
 });
 
 // Remove calendar
-apiRouter.delete("/calendars", verifyAuth, (req, res) => {
-  let user = users.find((user) => user.token === req.cookies.token);
+apiRouter.delete("/calendars", verifyAuth, async (req, res) => {
+  let user = await findUser("token", req.cookies.token);
   user.calendars = user.calendars.filter(
     (calendar) => calendar !== req.body.calendar
   );
+  await DB.updateUser(user);
   res.send({ calendars: user.calendars });
 });
+
+async function createUser(email, hasjedPassword) {
+  const user = {
+    email: email,
+    password: hasjedPassword,
+    token: uuid.v4(),
+    completedAssignments: [],
+    calendars: [],
+  };
+  await DB.addUser(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  if (field === "token") {
+    return DB.getUserByToken(value);
+  }
+  return DB.getUser(value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie("token", authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
