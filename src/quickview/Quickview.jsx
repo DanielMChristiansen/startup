42cddeb1bd08593622584fcb331143e8e50cd96b
DanielMChristiansen <danielmchristiansen@outlook.com>
2025-03-24 23:18:45 -0600

    Updated condition for frontend websocket

--- src/quickview/Quickview.jsx
+++ src/quickview/Quickview.jsx
@@ -102,9 +102,10 @@ function Quickview() {
 
     socket.onmessage = (event) => {
       const message = event.data;
+      console.log(`Message received: ${message}`);
       if (message === 'Assignment may be due soon') {
         if (9 < (new Date()).getHours() && (new Date()).getHours() < 24) { // Only show the notification if it's before midnight
-          if (ASSIGNMENTS.find(element => isIncompleteAndDueTonight(element))) { // No need to show an empty notification
+          if (ASSIGNMENTS.filter(assignment => isIncompleteAndDueTonight(assignment)).length > 0) { // No need to show an empty notification
             setAssignmentDueSoon(true);
           }
         }
